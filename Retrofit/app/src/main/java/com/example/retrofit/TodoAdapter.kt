4907e6171cd25e6cb54d2d81cf1d83f107dbb177
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-11-18 08:07:02 +0530

    Added retrofit

---
+++ Retrofit/app/src/main/java/com/example/retrofit/TodoAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.retrofit
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofit.databinding.ItemTodoBinding
+
+class TodoAdapter :RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
+    inner class TodoViewHolder(binding: ItemTodoBinding) :RecyclerView.ViewHolder(binding.root)
+    private val
+}
